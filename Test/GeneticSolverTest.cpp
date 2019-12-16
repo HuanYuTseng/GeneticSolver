4958d06ad3745ccc63a4056ae3a3759369638096
Huan-Yu Tseng <huanyutseng810330@gmail.com>
2019-12-16 21:06:05 +0800

    Add two test case
    Except normally test case, add two test cases: wrong size of parameters and null loss function pointer.

--- Test/GeneticSolverTest.cpp
+++ Test/GeneticSolverTest.cpp
@@ -14,6 +14,7 @@ int main() {
 	GeneticSolver gs;
 
 	std::vector<double> paras;
+	std::vector<double> parasFake;
 	std::vector<double> parasMax;
 	std::vector<double> parasMin;
 
@@ -23,14 +24,52 @@ int main() {
 		parasMin.push_back(10);
 	}
 
-	gs.setLossFunction(lossFunc);
+	std::cout << "Genetic Solver Test Start." << std::endl << std::endl;
+
+	std::cout << "Case 1 Wrong Size of Parameters" << std::endl;
+	
+	if (gs.setParamters(parasFake, parasMax, parasMin)){
+		std::cout << "Case 1 failed." << std::endl;
+		system("PAUSE");
+		return 1;
+	}
+	else {
+		std::cout << "Case 1 passed." << std::endl;
+		std::cout << "Case 1 Error Message: " << gs.getMsg() << std::endl;
+	}
 	gs.setParamters(paras, parasMax, parasMin);
 
-	std::vector<double> result = gs.solve();
-	std::cout << "The final values: " << std::endl;
-	for (const auto& value : result) {
-		std::cout << value << std::endl;
+	std::cout << std::endl << "Case 2 Null loss function pointer." << std::endl;
+
+	if (gs.setLossFunction(NULL)) {
+		std::cout << "Case 2 failed." << std::endl;
+		system("PAUSE");
+		return 1;
 	}
-	std::cout << std::endl << "Loss value: " << gs.getLoss() << std::endl;
+	else {
+		std::cout << "Case 2 passed." << std::endl;
+		std::cout << "Case 2 Error Message: " << gs.getMsg() << std::endl;
+	}
+
+	std::cout << std::endl << "Case 3 Normally Test." << std::endl;
+	gs.setLossFunction(lossFunc);
+	std::vector<double> result = gs.solve();
+
+	if (result.size() == paras.size()) {
+		std::cout << "Final values of parameters: " << std::endl << std::endl;
+		for (const auto& value : result) {
+			std::cout << value << std::endl;
+		}
+		std::cout << std::endl << "Loss value: " << gs.getLoss() << std::endl;
+
+		std::cout << "Case 3 passed." << std::endl;
+
+		std::cout << "Case 3 Message: " << gs.getMsg() << std::endl;
+	}
+	else {
+		std::cout << "Case 3 failed." << std::endl;
+	}
+
+	std::cout << std::endl << "Genetic Solver Test End." << std::endl;
 	system("PAUSE");
 }
